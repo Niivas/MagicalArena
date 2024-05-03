7054730c67c28c393987729fdd5bb0ba21b3d1c9
Nivas Reddy Nalla <nivas6900@gmail.com>
2024-05-03 21:17:58 +0530

    Add models

---
+++ src/main/java/org/magicalArena/models/Match.java
@@ -0,0 +1,44 @@
+package org.magicalArena.models;
+
+
+public class Match {
+    private Player attacker;
+    private Player defender;
+
+    private Player winner;
+
+    public Match(Player attacker, Player defender) {
+        this.attacker = attacker;
+        this.defender = defender;
+    }
+
+    public void fight() {
+        while (attacker.getHealth() > 0 && defender.getHealth() > 0) {
+            System.out.println("Attacker: " + attacker.getName() + " Defender: " + defender.getName());
+            int attackValue = attacker.attack();
+            System.out.println(attacker.getName() + " attacked " + defender.getName() + " with " + attackValue + " damage");
+            int defendValue = defender.defend();
+            System.out.println(defender.getName() + " defended with " + defendValue + " damage");
+            int damage = Math.max(0, attackValue - defendValue);
+            defender.takeDamage(damage);
+            System.out.println(defender.getName() + " took " + damage + " damage");
+            if (defender.getHealth() <= 0) {
+                winner = attacker;
+                break;
+            }
+            this.playerSwap();
+            System.out.println("------------------------------------------------------");
+        }
+        System.out.println("Match ended with winner: " + winner.getName());
+    }
+
+    public void playerSwap() {
+        Player temp = attacker;
+        attacker = defender;
+        defender = temp;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+}
---
+++ src/main/java/org/magicalArena/models/Player.java
@@ -0,0 +1,86 @@
+package org.magicalArena.models;
+public class Player {
+    private String name;
+    private int strength;
+    private int health;
+    private int attack;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    private Player(Builder builder) {
+        this.name = builder.name;
+        this.strength = builder.strength;
+        this.health = builder.health;
+        this.attack = builder.attack;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public int attack(){
+        int dieResult = (int) (Math.random() * 6 + 1);
+        return dieResult * this.attack;
+    }
+
+    public int defend(){
+        int dieResult = (int) (Math.random() * 6 + 1);
+        return dieResult * this.strength;
+    }
+
+    public void takeDamage(int damage){
+        this.health -= damage;
+    }
+
+    protected static class Builder {
+        private String name;
+        private int strength;
+        private int health;
+        private int attack;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder strength(int strength) {
+            this.strength = strength;
+            return this;
+        }
+
+        public Builder health(int health) {
+            this.health = health;
+            return this;
+        }
+
+        public Builder attack(int attack) {
+            this.attack = attack;
+            return this;
+        }
+
+        public void validate() {
+            if (name == null || name.isEmpty()) {
+                throw new IllegalArgumentException("Name cannot be null or empty");
+            }
+            if (strength < 0) {
+                throw new IllegalArgumentException("Strength cannot be negative");
+            }
+            if (health < 0) {
+                throw new IllegalArgumentException("Health cannot be negative");
+            }
+            if (attack < 0) {
+                throw new IllegalArgumentException("Attack cannot be negative");
+            }
+        }
+
+        public Player build() {
+            return new Player(this);
+        }
+    }
+}
